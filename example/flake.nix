4815cf959cd49420c1aee0c637f62f166a1e5125
Sridhar Ratnakumar <srid@srid.ca>
2023-02-23 11:23:32 -0500

    readme and such

--- example/flake.nix
+++ example/flake.nix
@@ -2,7 +2,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    nixid.url = "github:srid/nixid/init";
+    nixid.url = "github:srid/nixid";
   };
   outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
