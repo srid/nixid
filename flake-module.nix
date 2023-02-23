4b7bb107228b7b9fd84b399921898aa2e9abf0ad
Sridhar Ratnakumar <3998+srid@users.noreply.github.com>
2023-02-23 18:57:44 -0500

    add `show-trace` option (#4)

--- flake-module.nix
+++ flake-module.nix
@@ -8,34 +8,49 @@ let
 in
 {
   options = {
-    perSystem = mkPerSystemOption
-      ({ config, self', inputs', pkgs, system, ... }:
-        {
-          options = {
-            expr = lib.mkOption {
-              type = types.raw;
-              description = ''
-                The expression to be evaluated.
+    perSystem = mkPerSystemOption ({ config, self', inputs', pkgs, system, ... }: {
+      options = {
+        nixid = lib.mkOption {
+          default = { };
+          type = types.submodule {
+            options = {
+              expr = lib.mkOption {
+                type = types.raw;
+                description = ''
+                  The expression to be evaluated.
 
-                It must be convertable to a string using `builtins.toString`.
-              '';
+                  It must be convertable to a string using `builtins.toString`.
+                '';
+              };
+              show-trace = lib.mkOption {
+                type = types.bool;
+                default = false;
+                description = ''
+                  Whether to show the stack trace.
+                '';
+              };
             };
           };
-          config = {
-            packages.default = pkgs.writeShellApplication {
-              name = "run";
-              text = ''
-                function runIt() {
-                  cat "$(nix build .#expr-output --show-trace --no-link --print-out-paths)"
-                }
-                export -f runIt
-                ${lib.getExe pkgs.watchexec} -e nix runIt
-              '';
-            };
-            packages.expr-output =
-              pkgs.writeText "expr" (builtins.toString config.expr + "\n");
-          };
-        });
+        };
+
+      };
+      config = {
+        packages.default = pkgs.writeShellApplication {
+          name = "run";
+          text = ''
+            function runIt() {
+              set -euo pipefail
+              OUT="$(nix build .#expr-output ${if config.nixid.show-trace then "--show-trace" else ""} --no-link --print-out-paths)"
+              cat "$OUT"
+            }
+            export -f runIt
+            ${lib.getExe pkgs.watchexec} -e nix runIt
+          '';
+        };
+        packages.expr-output =
+          pkgs.writeText "expr" (builtins.toString config.nixid.expr + "\n");
+      };
+    });
   };
 
 }
